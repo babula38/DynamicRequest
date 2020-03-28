09bd565b118fe038129aa1a3ef1edce3a3718ba2
Babu <babula38@gmail.com>
2020-03-28 17:25:49 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicRequest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok();
+
+        [HttpPost]
+        public void Post(dynamic request)
+        {
+            //json request
+            //{
+            // "Foo":"Value"
+            //}
+            var expected = request.Foo;
+            //expected should contain the value for Foo.
+            //But the object structure is different
+        }
+
+    }
+}
